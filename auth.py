ab8fcce7582e0c39be252771a46ff538c596267d
kkailaasa <138176796+kkailaasa@users.noreply.github.com>
2024-07-17 17:48:56 +0300

    added auth phone_number caching

--- auth.py
+++ auth.py
@@ -1,10 +1,18 @@
-from keycloak_utils import get_user_by_phone
+from keycloak import get_user_by_phone
 
+redis_client = redis.StrictRedis(host='localhost', port=6379)
+# 7 days
+AUTH_TIME_WINDOW = 7 * 24 * 60 * 60
 
 def is_user_authorized(phone_number):
     phone_number = phone_number.split(':')[1].strip()
-    users = get_user_by_phone(phone_number)
-    if len(users) == 1:
-        return True
+    key = f"auth_phone:{phone_number}"
+    auth_user = redis_client.get(key)
+
+    if auth_user is None:
+        users = get_user_by_phone(phone_number)
+        if len(users) == 1:
+            redis_client.setex(key, AUTH_TIME_WINDOW, 1)
+            return True
     return False
-    
+return True
