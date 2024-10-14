d08fa60bc84df3507c95939089b18d69b299a420
kkailasa <kenyakailasa@gmail.com>
2024-10-15 00:42:23 +0300

    chore: cleaning redis dependencies on ecitizen authorization function

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,4 @@
-from .ecitizen_auth import is_user_authorized, create_keycloak_admin, get_user_by_email
+from .ecitizen_auth import create_keycloak_admin, get_user_by_email
 from .dify_chat import ChatService
 from .twillio_auth import MessagingService
 
