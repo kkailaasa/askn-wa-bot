b0903e1ce9b819987408aacc6a1e160696a9349d
kkailasa <kenyakailasa@gmail.com>
2024-10-15 11:02:33 +0300

    removed whatsapp from prefix in dify user sent

--- tasks/celery_tasks.py
+++ tasks/celery_tasks.py
@@ -1,15 +1,34 @@
+from celery import Celery
+from services.dify_chat import ChatService
+from services.twillio_auth import MessagingService
+from utils.redis_helpers import is_rate_limited
+from core.config import settings
+import logging
+import traceback
+
+logger = logging.getLogger(__name__)
+
+# Initialize the Celery app
+app = Celery('tasks', broker=settings.REDIS_URL, backend=settings.REDIS_URL)
+
+app.conf.update(
+    broker_connection_retry_on_startup=True,
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+)
+
+chat_service = ChatService()
+messaging_service = MessagingService()
+
 @app.task
 def process_question(Body: str, From: str):
     logger.info("Processing question")
     try:
         # Remove 'whatsapp:' prefix from the phone number
         From = From.replace('whatsapp:', '')
-
-        #Authorization Celery Task
-        #if not is_user_authorized(From):
-        #    logger.info(f"User not present with phone number {From}")
-        #    messaging_service.send_message(From, "Signup to continue chatting with Ask Nithyananda, please visit +1 2518100108")
-        #    return
 
         if is_rate_limited(From):
             logger.info(f"Rate limit exceeded for {From}")
