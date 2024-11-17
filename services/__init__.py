f38955c48199255c5a0acdb2c7d07f547be60986
kkailasa <kenyakailasa@gmail.com>
2024-11-17 15:25:10 +0300

    some errors in keycloak operations exports

--- services/__init__.py
+++ services/__init__.py
@@ -6,10 +6,17 @@ from .rate_limiter import RateLimiter
 from .email_service import EmailService
 
 from .ecitizen_auth import (
-    auth_service,
-    ECitizenAuthService,
-    KeycloakTokenManager,
-    KeycloakHealthMonitor
+    get_user_by_email_or_username,
+    get_user_by_phone_or_username,
+    create_user_with_phone,
+    add_phone_attributes_to_user,
+    verify_email,
+    generate_otp,
+    store_otp,
+    verify_otp,
+    store_temp_data,
+    get_temp_data,
+    delete_temp_data
 )
 
 from .sequence_manager import (
@@ -20,15 +27,28 @@ from .sequence_manager import (
     STEP_VALIDATORS
 )
 
+# Create an instance of the auth service
+class EcitzenAuthService:
+    def __init__(self):
+        self.create_keycloak_admin = create_keycloak_admin
+        self.get_user_by_email_or_username = get_user_by_email_or_username
+        self.get_user_by_phone_or_username = get_user_by_phone_or_username
+        self.create_user_with_phone = create_user_with_phone
+        self.add_phone_attributes_to_user = add_phone_attributes_to_user
+        self.verify_email = verify_email
+        self.generate_otp = generate_otp
+        self.store_otp = store_otp
+        self.verify_otp = verify_otp
+        self.store_temp_data = store_temp_data
+        self.get_temp_data = get_temp_data
+        self.delete_temp_data = delete_temp_data
+
 # List all public exports
 __all__ = [
     # Services
     'ChatService',
     'MessagingService',
-    'EmailService',
-    'RateLimiter',
-    'auth_service',
-    'ECitizenAuthService',
+    'EcitzenAuthService',
 
     # Sequence Management
     'sequence_manager',
@@ -38,6 +58,19 @@ __all__ = [
     'STEP_VALIDATORS',
 
     # Auth Related
-    'KeycloakTokenManager',
-    'KeycloakHealthMonitor'
+    'get_user_by_email_or_username',
+    'get_user_by_phone_or_username',
+    'create_user_with_phone',
+    'verify_email',
+    'generate_otp',
+    'store_otp',
+    'verify_otp',
+    'store_temp_data',
+    'get_temp_data',
+    'delete_temp_data'
+
+    # Data Management
+    'store_temp_data',
+    'get_temp_data',
+    'delete_temp_data'
 ]
